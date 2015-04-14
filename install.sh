992dd8d763069962710119f4350db801b3a4c336
pistacchio <pistacchio@gmail.com>
2015-04-14 18:52:30 +0200

    + update

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 mkdir -p ~/.izanagi/formulas
 mkdir -p ~/.izanagi/bin
+mkdir -p ~/.izanagi/cache
+
 wget https://raw.githubusercontent.com/pistacchio/izanagi/master/src/izanagi.py -O ~/.izanagi/bin/izanagi.py
+wget https://raw.githubusercontent.com/pistacchio/izanagi/master/src/config -O ~/.izanagi/config
 chmod +x ~/.izanagi/bin/izanagi.py
 ln -s ~/.izanagi/bin/izanagi.py /usr/local/bin/izanagi
 
+izanagi update
--- src/izanagi.py
+++ src/izanagi.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 
 import argparse
+import datetime
+import json
+import imp
 import os
+import re
 import stat
 import sys
-
-from distutils.dir_util import copy_tree
+import urllib2
 
 # TOP LEVEL CONSTANTS
 IZANAGI_HOME               = os.path.join(os.path.expanduser("~"), '.izanagi')
 IZANAGI_LOCAL_FORMULA_PATH = os.path.join(IZANAGI_HOME, 'formulas')
+IZANAGI_CACHE_PATH         = os.path.join(IZANAGI_HOME, 'cache')
+IZANAGI_CACHE_FILE         = os.path.join(IZANAGI_CACHE_PATH, 'cache')
+IZANAGI_CONFIG_FILE        = os.path.join(IZANAGI_HOME, 'config')
+
+GITHUB_API_BASE_URL        = 'https://api.github.com/repos/'
+
+REGEX_FORMULA_FROM_URL     = re.compile(r'formulas/(.*?)/')
+
+config = imp.load_source('config', IZANAGI_CONFIG_FILE)
 
 # CLI ARGUMENT PARSER
 parser     = argparse.ArgumentParser(prog="izanagi")
@@ -24,6 +36,8 @@ list_parser = subparsers.add_parser('list', help='list available formulas')
 
 search_parser = subparsers.add_parser('search', help='search for formula')
 search_parser.add_argument('search_string',    action='store', help='formula to search for', type=str)
+
+update_parser = subparsers.add_parser('update', help='update formula data from remote repositories')
 
 args = vars(parser.parse_args())
 
@@ -82,4 +96,27 @@ if __name__ == "__main__":
 
     if args['command'] == 'search':
         search_for_formula(args['search_string'])
+        sys.exit(0)
+
+    if args['command'] == 'update':
+        caches = []
+        for remote_repo in config.remote_repos:
+            remote_api_url = GITHUB_API_BASE_URL + remote_repo.replace('https://github.com/', '').replace('.git', '') + '/git/trees/master?recursive=1'
+            response       = urllib2.urlopen(remote_api_url)
+
+            json_formulas  = json.loads(response.read())
+            formulas       = set()
+            for formula in [f['path'] for f in json_formulas['tree'] if f['path'].startswith('formulas')]:
+                match = REGEX_FORMULA_FROM_URL.match(formula)
+                if match:
+                    formulas.add(match.groups()[0])
+
+            formulas = list(formulas)
+            caches.append({
+                'repository': remote_repo,
+                'updated':    str(datetime.datetime.now()),
+                'formulas':   formulas
+            })
+
+        json.dump(caches, open(IZANAGI_CACHE_FILE, 'w+'))
         sys.exit(0)
