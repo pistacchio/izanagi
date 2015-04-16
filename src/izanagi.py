4e01a3756fc7015e9a485fb3a49da9db82d124a7
pistacchio <pistacchio@gmail.com>
2015-04-16 10:07:43 +0200

    + merged from dr-wahrheit pull request

--- src/izanagi.py
+++ src/izanagi.py
@@ -10,6 +10,8 @@ import stat
 import sys
 import urllib2
 
+from distutils.dir_util import copy_tree
+
 # TOP LEVEL CONSTANTS
 IZANAGI_HOME               = os.path.join(os.path.expanduser("~"), '.izanagi')
 IZANAGI_LOCAL_FORMULA_PATH = os.path.join(IZANAGI_HOME, 'formulas')
@@ -26,6 +28,8 @@ config = imp.load_source('config', IZANAGI_CONFIG_FILE)
 #######################
 # CLI ARGUMENT PARSER #
 #######################
+
+# TODO shortcut options like "i" for "install"
 
 parser     = argparse.ArgumentParser(prog="izanagi")
 subparsers = parser.add_subparsers(dest='command', help='commands')
@@ -61,6 +65,13 @@ def get_formula(formula_name):
 
     return formulas
 
+def get_remote_tree(remote_repo_url):
+    # TODO better creation of the url
+    remote_api_url = GITHUB_API_BASE_URL + remote_repo_url.replace('https://github.com/', '').replace('.git', '') + '/git/trees/master?recursive=1'
+    response       = urllib2.urlopen(remote_api_url)
+
+    return json.loads(response.read())
+
 
 def install_formula(formula_name, destination_path, options=''):
     formulas = get_formula(formula_name)
@@ -70,42 +81,59 @@ def install_formula(formula_name, destination_path, options=''):
         sys.exit(1)
 
     if len(formulas) > 1:
-        formula_list = []
+        repositories = []
         print 'Formula found on multiple repositories:'
 
         # show local in first position if exists
         if 'local' in formulas:
-            formula_list.append('local')
+            repositories.append('local')
 
-        formula_list += [f for f in formulas if f != 'local']
+        repositories += [f for f in formulas if f != 'local']
 
-        for position, formula in enumerate(formula_list):
+        for position, formula in enumerate(repositories):
             print '[%d] %s (%s)' % (position, formula, formulas[formula])
 
         while True:
-            origin = raw_input('Please select one: ')
-            if origin in [str(i) for i in range(len(formula_list))]:
+            raw_origin = raw_input('Please select one: ')
+            if raw_origin in [str(i) for i in range(len(repositories))]:
+                origin = repositories[int(raw_origin)]
                 break
             print 'Invalid choice.'
+    else:
+        origin = formulas.keys[0]
 
-        if origin == 'local':
-            formula_path = formulas['local']
-            skel_path    = os.path.join(formula_path, 'skel')
-            if os.path.exists(skel_path):
-                copy_tree(skel_path, destination_path)
+    if origin == 'local':
+        formula_path = formulas['local']
+    else:
+        # TODO download all the files to a temp directory and set it as formula_path
+        repository = [r for r in json.load(open(IZANAGI_CACHE_FILE)) if r['repository'] == origin][0]
+        all_remote_files = get_remote_tree(repository['repository_url'])['tree']
+        remote_files = [rf for rf in all_remote_files if rf['path'].startswith('formulas/' + formula_name)]
+        for remote_file in remote_files:
+            # TODO better creation of the url
+            url = repository['repository_url'].replace('.git', '').replace('github.com', 'raw.githubusercontent.com') + '/master/' + remote_file['path']
+            url = 'https://raw.githubusercontent.com/pistacchio/izanagi/master/'
 
-            install_file = os.path.join(formula_path, 'install')
-            if os.path.exists(install_file):
-                try:
-                    file_stats = os.stat(install_file)
-                    os.chmod(install_file, file_stats.st_mode | stat.S_IEXEC)
-                except:
-                    pass
+            # TODO complete the installation
 
-            os.system('"%s" "%s" %s' % (install_file, destination_path, options))
+    skel_path    = os.path.join(formula_path, 'skel')
+    if os.path.exists(skel_path):
+        copy_tree(skel_path, destination_path)
+
+    install_file = os.path.join(formula_path, 'install')
+    if os.path.exists(install_file):
+        try:
+            file_stats = os.stat(install_file)
+            os.chmod(install_file, file_stats.st_mode | stat.S_IEXEC)
+        except:
+            pass
+
+    os.system('"%s" "%s" %s' % (install_file, destination_path, options))
+
 
 
 def list_formulas(search_string=''):
+    # show local in first position if exists
     local_formulas = os.listdir(IZANAGI_LOCAL_FORMULA_PATH)
     if search_string:
         local_formulas = [f for f in local_formulas if search_string in f]
@@ -134,11 +162,9 @@ def search_for_formula(search_string):
 def update_cache():
     caches = []
     for remote_repo, remote_repo_url in config.remote_repos.iteritems():
-        remote_api_url = GITHUB_API_BASE_URL + remote_repo_url.replace('https://github.com/', '').replace('.git', '') + '/git/trees/master?recursive=1'
-        response       = urllib2.urlopen(remote_api_url)
+        json_formulas = get_remote_tree(remote_repo_url)
+        formulas = set()
 
-        json_formulas  = json.loads(response.read())
-        formulas       = set()
         for formula in [f['path'] for f in json_formulas['tree'] if f['path'].startswith('formulas')]:
             match = REGEX_FORMULA_FROM_URL.match(formula)
             if match:
